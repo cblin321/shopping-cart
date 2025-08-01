1d91d731f1bba95e5122fc057d1118be275be930
Chris Lin <chrislin90210@gmail.com>
2025-08-01 02:27:48 -0500

    work on data fetching

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,33 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Hero from "./components/Hero/Hero" 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Shop from "./Shop/Shop"
+import Cart from "./components/Cart/Cart"
+
+console.log("fdjskfjkl")
+const routes = [
+    {
+        path: "/",
+        element: <App></App>
+    },
+
+    {
+        path: "shop",
+        element: <Shop></Shop>
+    },
+
+    {
+        path: "cart",
+        element: <Cart></Cart>
+    }
+]
+const router = createBrowserRouter(routes)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <p>fdjkslfjsl</p>
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
